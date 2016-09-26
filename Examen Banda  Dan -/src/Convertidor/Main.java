dba24694bdebd6afce41f1555171dbc0c38b9b43
dan2344 <america_emerson7@hotmail.com>
2016-09-26 08:39:38 -0500

    Correción del examen

---
+++ Examen Banda  Dan -/src/Convertidor/Main.java
@@ -0,0 +1,16 @@
+
+package Convertidor;
+
+import Convertidor.Convertidor;
+
+/**
+ * Ejercicio: 
+ * - Conversor
+ *@author:  RubioHaro
+ */
+public class Main {
+ public static void main(String[] args) {
+		Convertidor calculadora = new Convertidor();
+		calculadora.setVisible(true);
+	}   
+}
